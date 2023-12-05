98718065cfe9dd947c38bb9e6d191a81ccbf8800
werdy <69005656+werdylp@users.noreply.github.com>
2023-12-05 11:24:57 +0100

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace ukolnicek;
+
+public partial class MainPage : ContentPage
+{
+
+	public List<ukol> Seznam { get; set; }
+		
+	public MainPage()
+	{
+		InitializeComponent();
+		
+		//vypis.ItemsSource = seznam;
+		Seznam = new List<ukol>();
+		vypis.BindingContext = this;
+        
+    }
+
+	private void novy(object sender, EventArgs e)
+	{
+		ukol u = new ukol(nazev.Text, dat.Date, (byte)pru.SelectedIndex);
+		Seznam.Add(u);
+
+		vypis.BindingContext = null;
+		vypis.BindingContext = this;
+	}
+}
---
+++ ukol.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ukolnicek
+{
+    public class ukol
+    {
+        string nazev;
+        DateTime? termin;
+
+        byte prubeh;
+
+        public string Nazev { get => nazev; /* set => nazev = value;*/ }
+        public string Termin { get => termin != null ? ((DateTime)termin).ToString("dd.MM.yyyy") : ""; }//otazdnik povoli hodnotu null
+
+
+
+        public string Prubeh
+        {
+            get
+            {
+                switch (prubeh) 
+                {
+                    case 0:
+                        return "zadano";
+                    case 1:
+                        return "rozpracovano";
+                    case 3:
+                        return "splněno";
+
+                    default:
+                        return "";
+                }
+            }
+        }
+
+        public ukol(string nazev,DateTime termin,byte prubeh) { 
+        
+            this.nazev = nazev;
+            this.termin = termin;
+            this.prubeh = prubeh;
+
+        }
+    }
+}
